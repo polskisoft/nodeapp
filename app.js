2d2293b939e95a7f00a7a1a3c8806ced676da45b
Paweł Płachciak <pawelarnoldpl@gmail.com>
2024-11-24 21:30:29 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port=3000
+
+const app =express()
+
+app.get('/',(req, res)=>{
+    res.send('Node.js app')
+})
+app.get('/about',(req, res)=>{
+    res.send('About this app...')
+})
+
+app.listen(port)
